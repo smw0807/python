333e0052a9c91b1fb59c8e1f47520cc887641584
smw0807 <smw0807@gmail.com>
2025-10-26 22:11:09 +0900

    강의 : 모듈 사용

---
+++ panda/part2/module.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pi)
+print(math.sqrt(16))
+print(math.pow(2, 3))
+print(math.log(10))
+print(math.log10(10))
+print(math.log2(10))
+print(math.exp(1))
+print(math.sin(math.pi/2))
+print(math.cos(math.pi/2))
+print(math.tan(math.pi/2))
+print(math.factorial(5))
