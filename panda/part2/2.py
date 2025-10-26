eb615e3574f480f91460bb5cb4f55ad32b2b2fdc
smw0807 <smw0807@gmail.com>
2025-10-27 04:54:32 +0900

    zfill

--- panda/part2/2.py
+++ panda/part2/2.py
@@ -1,2 +1,13 @@
 for i in range(1, 20):
   print('T' + str(i).zfill(5))
+
+
+a = []
+
+def make_serial(n):
+  return 'T' + str(n).zfill(5)
+
+for i in range(1, 20):
+  a.append(make_serial(i))
+
+print(a)
