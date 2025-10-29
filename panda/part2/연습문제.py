26d12cd45e4f32c7de827ba73f82530de61d9c0f
smw0807 <smw0807@gmail.com>
2025-10-29 22:46:32 +0900

    연습문제

---
+++ panda/part2/연습문제.py
@@ -0,0 +1,32 @@
+# 정수 10의 자료형을 확인하는 코드
+print(type(10))
+
+# x = 3.99일 때, x를 정수로 변환하는 코드
+x = 3.99
+print(int(x))
+
+# 7 // 2와 7 / 2의 결과를 출력하는 코드
+print(7 // 2) # 몫
+print(7 / 2) # 나누기
+
+# s = 3.14를 실수로 변환하는 코드
+s = 3.14
+print(float(s))
+
+# h = 'Hello'읠 0번과 -1번 인덱스 출력
+h = 'Hello'
+print(h[0]) # H
+print(h[-1]) # o
+
+# a = '송민우'일 때 a의 길이
+a = '송민우'
+print(len(a))
+
+# w = 'Hello, World'에서 마지막 6글자만 출력
+w = 'Hello, World'
+print(w[-6:])
+
+# b = '12345'일 때 홀수 번째 문자만 출력
+b = '12345'
+print(b[::2])
+
