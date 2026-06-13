d539fb33df362c667332b434e763ea2f030b4def
smw0807 <smw0807@gmail.com>
2026-06-13 20:10:59 +0900

    멀티 프로세싱

---
+++ lecture/02/04-1-cpu-basic.py
@@ -0,0 +1,26 @@
+import time
+import os
+import threading
+
+nums = [50, 63, 32]
+
+def cpu_bound_func(num):
+  print(f"{os.getpid()} process | {threading.get_ident()} thread")
+  numbers = range(1, num)
+  total = 1
+  for i in numbers:
+    for j in numbers:
+      for k in numbers:
+        total *= i * j * k
+  return total
+
+def main():
+  [cpu_bound_func(num) for num in nums] # 11 sec
+  # for num in nums:
+  #   cpu_bound_func(num)
+
+if __name__ == "__main__":
+  start = time.time()
+  main()
+  end = time.time()
+  print(end - start)
