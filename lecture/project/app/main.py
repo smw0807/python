4bd337f30f0fa42decea3fba5fb2f91e33aa83cb
smw0807 <smw0807@gmail.com>
2026-06-17 21:05:17 +0900

    jinja2 템플릿

--- lecture/project/app/main.py
+++ lecture/project/app/main.py
@@ -1,20 +1,20 @@
-from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+#from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+# 미들웨어
+# app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
-@app.get('/hello')
-def read_fastapi_hello():
-  return {"Hello": "Fast API"}
+# html 템플릿
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-@app.get("/items/{item_id}/{item_name}")
-def read_item(item_id: int, item_name: str, q: Optional[str] = None):
-  return {"item_id": item_id, "item_name": item_name, "q": q}
-
-@app.get("/item-name/{item_name}")
-def read_item_name(item_name: str):
-  return {"item_name": item_name}
+@app.get("/item/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+  # 템플릿 렌더링
+  return templates.TemplateResponse("item.html", { "request": request, "id": id , "name": "songminwoo"})
