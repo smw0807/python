90386acb69b7702d604ba59acd80083da79273cd
smw0807 <smw0807@gmail.com>
2026-06-18 23:20:33 +0900

    static 제거

--- lecture/project/app/main.py
+++ lecture/project/app/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-#from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
+print('BASE_DIR', BASE_DIR)
 
 app = FastAPI()
 
-# 미들웨어
-# app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
-
 # html 템플릿
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+templates_dir = BASE_DIR / "templates"
+print('templates_dir', templates_dir)
+templates = Jinja2Templates(directory=templates_dir)
 
 @app.get("/item/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
   # 템플릿 렌더링
-  return templates.TemplateResponse("item.html", { "request": request, "id": id , "name": "songminwoo"})
+  return templates.TemplateResponse("./item.html", { "request": request, "id": id , "name": "songminwoo"})
