9ec5b52f33fad32325515160757eea69471c8844
smw0807 <smw0807@gmail.com>
2026-06-22 06:15:16 +0900

    매개변수 타입

--- fast-api/routers/item.py
+++ fast-api/routers/item.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Query, HTTPException
 from enum import Enum
 from typing import Optional
+from pydantic import BaseModel
 
 router = APIRouter(prefix='/items', tags=['items'])
 
@@ -8,6 +9,12 @@ class ModelName(str, Enum):
   alexnet = "alexnet"
   lcnet = "lcnet"
   resnet = "resnet"
+
+class Item(BaseModel):
+  name: str
+  description: Optional[str] = None
+  price: float
+  tax: Optional[float] = None
 
 # @router.get('/{item_id}')
 # async def read_item(item_id):
@@ -25,5 +32,9 @@ async def read_items(skip: int = 0, limit: int = 10):
 async def search_items(keyword: str = Query(default=None), skip: int = 0, limit: int = 10):
   if keyword is None:
     raise HTTPException(status_code=400, detail="Keyword is required")
-    
+
   return {"keyword": keyword, "skip": skip, "limit": limit}
+
+@router.post('/create_item/')
+async def create_item(item: Item):
+  return item
