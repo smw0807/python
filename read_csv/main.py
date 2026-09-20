b3c66168a4bf9f36f5369c804c3a93dcc3047494
smw0807 <smw0807@gmail.com>
2026-09-20 16:33:44 +0900

    test

--- read_csv/main.py
+++ read_csv/main.py
@@ -3,9 +3,19 @@ import pandas as pd
 def read_csv(file_path: str) -> pd.DataFrame:
   return pd.read_csv(file_path)
 
+def transform_data(df: pd.DataFrame) -> None:
+  # 데이터 컬럼 배열로 수집
+  columns = df.columns.tolist()
+  print(columns)
+
+  for column in columns:
+    print(f"{column}: {df[column].dtype}")
+
+
+
 
 if __name__ == "__main__":
   df = read_csv("data/raw/customers.csv")
-  print(df.head())
+  transform_data(df)
 
 
